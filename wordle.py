ec41da77534889fc461e2a698d4e7827442c3ce9
frnfnts <frn.fnts@gmail.com>
2024-05-08 18:51:19 +0900

    refactor

--- wordle.py
+++ wordle.py
@@ -2,31 +2,34 @@ import itertools
 
 
 def main():
-    letters = "norti"
-    not_at = ["n", "o", "r", "tn", "t"]
+    letters = ""
+    not_at = ["", "", "", "", ""]
     exact_at = ["", "", "", "", ""]
     may_use = ""
+
+    vague_count = letters.count("-")
     p = itertools.permutations(letters, 5)
     memo = set()
-    for i in p:
+    for word in p:
         ng = False
-        for at, ii in enumerate(i):
-            if ii in not_at[at]:
+        for at, letter in enumerate(word):
+            if letter in not_at[at]:
                 ng = True
                 break
-            if exact_at[at] and ii != exact_at[at]:
+            if exact_at[at] and letter != exact_at[at]:
                 ng = True
                 break
-        if not ng:
-            word = "".join(i)
-            x_count = word.count("-")
-            for sub in itertools.combinations(may_use, x_count):
-                replaced_word = word
-                for s in sub:
-                    replaced_word = replaced_word.replace("-", s, 1)
-                if replaced_word not in memo:
-                    memo.add(replaced_word)
-                    print(replaced_word)
+        if ng:
+            continue
+
+        pp = itertools.product(may_use, repeat=vague_count)
+        for may_use_letters in pp:
+            concrete_word = "".join(word)
+            for letter in may_use_letters:
+                concrete_word = concrete_word.replace("-", letter, 1)
+            if concrete_word not in memo:
+                memo.add(concrete_word)
+                print(concrete_word)
 
 
 if __name__ == "__main__":
