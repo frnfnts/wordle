229bfcc9428b23cca0f0d5807c3f8ea661b6e120
frnfnts <frn.fnts@gmail.com>
2024-05-08 17:50:34 +0900

    exact at

--- wordle.py
+++ wordle.py
@@ -4,11 +4,15 @@ import itertools
 def main():
     letters = "norti"
     not_at = ["n", "o", "r", "tn", "t"]
+    exact_at = ["", "", "", "", ""]
     p = itertools.permutations(letters, 5)
     for i in p:
         ng = False
         for at, ii in enumerate(i):
             if ii in not_at[at]:
+                ng = True
+                break
+            if exact_at[at] and ii != exact_at[at]:
                 ng = True
                 break
         if not ng:
