b3164267c5f4be5a94326a0fac144fa7fa061687
frnfnts <frn.fnts@gmail.com>
2024-05-27 13:27:55 +0900

    upgrade

--- wordle.py
+++ wordle.py
@@ -5,9 +5,9 @@ ALL_LETTERS = list("abcdefghijklmnopqrstuvwxyz")
 
 
 def main():
-    letters = ""
     not_at = ["", "", "", "", ""]
     exact_at = ["", "", "", "", ""]
+    use = ""
     not_use = ""
     may_use = ""
 
@@ -16,29 +16,31 @@ def main():
         for letter in not_use:
             may_use.remove(letter)
 
-    vague_count = letters.count("-")
-    p = itertools.permutations(letters, 5)
+    vague_count = 5 - sum([1 for x in exact_at if x])
     memo = set()
-    for word in p:
+    pp = itertools.product(may_use, repeat=vague_count)
+    for may_use_letters in pp:
+        # use が指定されている場合、その文字が含まれていないものはスキップ
+        if use and not any([x in use for x in may_use_letters]):
+            continue
+
+        concrete_word = "".join(map(lambda x: x if x else "-", exact_at))
+        for letter in may_use_letters:
+            concrete_word = concrete_word.replace("-", letter, 1)
+
+        # not_at が指定されている場合、その文字が含まれているものはスキップ
         ng = False
-        for at, letter in enumerate(word):
-            if letter in not_at[at]:
-                ng = True
-                break
-            if exact_at[at] and letter != exact_at[at]:
+        for letter, not_at_letters in zip(concrete_word, not_at):
+            if not_at_letters and letter in not_at_letters:
                 ng = True
                 break
         if ng:
             continue
 
-        pp = itertools.product(may_use, repeat=vague_count)
-        for may_use_letters in pp:
-            concrete_word = "".join(word)
-            for letter in may_use_letters:
-                concrete_word = concrete_word.replace("-", letter, 1)
-            if concrete_word not in memo:
-                memo.add(concrete_word)
-                print(concrete_word)
+        # 出力
+        if concrete_word not in memo:
+            memo.add(concrete_word)
+            print(concrete_word)
 
 
 if __name__ == "__main__":
